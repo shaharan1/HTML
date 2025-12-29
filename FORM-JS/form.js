15a26e688f964f9f3e5b8376540b8125e34c72c0
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2025-12-29 22:13:55 +0600

    add

--- FORM-JS/form.js
+++ FORM-JS/form.js
@@ -1,28 +1,29 @@
-function sendForm(event){
+function sendForm(event) {
+    event.preventDefault();
 
-     let id=document.getElementById("id").value;
-    let name=document.getElementById("name").value;
-    let email= document.getElementById("email").value;
-    let password=document.getElementById("password").value;
-     let address=document.getElementById("address").value;
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let address = document.getElementById("address").value;
     //   let round=document.getElementById("round").value;
     //         let gender=document.getElementById("gender").value;
     //   let hobby=document.getElementById("hobby").value;
 
 
 
-    let outPut= "ID: " + id + "\n";
-outPut= "Name: " + name + "\n";
-outPut= "Email: " + email + "\n";
-outPut= "Password: " + password + "\n";
-outPut= "Address: " + address + "\n";
+    let outPut = "ID: " + id + "\n";
+    outPut += "Name: " + name + "\n";
+    outPut += "Email: " + email + "\n";
+    outPut += "Password: " + password + "\n";
+    outPut += "Address: " + address + "\n";
 }
 
 
 
 
 
-let newWindow = window.open("",'_blank');
+let newWindow = window.open("", '_blank');
 newWindow.document.writeln("<pre>" + outPut + "</pre>");
 
 
